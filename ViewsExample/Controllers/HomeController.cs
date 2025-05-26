b83393555a319a3a7501b9156dfb8f1cb3c950bf
Ramjan Khan <khanramjan001@gmail.com>
2025-05-26 23:42:33 +0600

    Add project files.

---
+++ ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    [Route("home")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
